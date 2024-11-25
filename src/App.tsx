54f6175e3ce9d6f0c65c317d2946e933324df435
Andrew Johnson <johnsonrobertandrew@gmail.com>
2024-11-25 10:28:19 -0600

    added logos and added more sports

--- src/App.tsx
+++ src/App.tsx
@@ -7,73 +7,36 @@ import { useEffect, useState } from "react";
 import colors from "tailwindcss/colors";
 import { Analytics } from "@vercel/analytics/react";
 
-import mapboxgl from "mapbox-gl";
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiYXJqb2huc29uOTciLCJhIjoiY2xsZ3Vka3lxMTQxZTNkcGs4OHZtZjN3bSJ9.Pg1GGq9hS71uUKQfZ8Fm4A";
+import { useQueryState } from "nuqs";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { getGameLocations } from "./lib";
 
-async function getCityCoords(city: string) {
-  const res = await fetch(
-    `${mapboxgl.baseApiUrl}/geocoding/v5/mapbox.places/${encodeURIComponent(
-      city
-    )}.json?access_token=${mapboxgl.accessToken}&country=US,CA`
-  );
-  const { features } = await res.json();
-
-  const [lon, lat] = features[0].center;
-
-  return { lon, lat };
-}
-
-async function getGameLocations(sport: string) {
-  const res = await fetch(
-    `https://scores.weaklytyped.com/api/v1/sports/${sport}/events`
-  );
-  const data = await res.json();
-
-  const locations: {
-    label: string;
-    home: { name: string; lon: number; lat: number };
-    away: { name: string; lon: number; lat: number };
-  }[] = [];
-
-  if (data.scores) {
-    for (const score of data.scores) {
-      const home = await getCityCoords(
-        `${score.venue.address.city},${score.venue.address.state ?? ""}`
-      );
-      const away = await getCityCoords(`${score.teams.awayTeam.location}`);
-      locations.push({
-        label: `${score.teams.awayTeam.abbrev} @ ${score.teams.homeTeam.abbrev}`,
-        home: { name: score.teams.homeTeam.location, ...home },
-        away: { name: score.teams.awayTeam.location, ...away },
-      });
-    }
-  }
-
-  return locations;
-}
+const sports = {
+  mlb: "MLB",
+  nfl: "NFL",
+  nba: "NBA",
+  ncaaf: "NCAA College Football Top 25",
+  ncaam: "NCAA College Basketball Top 25",
+};
 
 export default function App() {
+  const [sport, setSport] = useQueryState("sport", {
+    defaultValue: "nfl",
+  });
+
   const [loading, setLoading] = useState(true);
-  const [sport, setSport] = useState<"mlb" | "nfl">("mlb");
-  const [_root, setRoot] = useState<am5.Root | null>(null);
 
   useEffect(() => {
+    const root = am5.Root.new("chartdiv");
+
     const setupMap = async () => {
-      /* Chart code */
-      // Create root element
-      // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-      if (_root) {
-        _root.dispose();
-      }
-      if (am5.registry.rootElements.length && _root) {
-        am5.registry.rootElements.forEach((e) => e.dispose());
-        _root.dispose();
-        setRoot(null);
-      }
       setLoading(true);
-      const root = am5.Root.new("chartdiv");
-      setRoot(root);
+
       // Set themes
       // https://www.amcharts.com/docs/v5/concepts/themes/
       const customTheme = am5.Theme.new(root);
@@ -94,29 +57,10 @@ export default function App() {
           projection: am5map.geoMercator(),
         })
       );
-      const cont = chart.children.push(
-        am5.Container.new(root, {
-          layout: root.horizontalLayout,
-          x: 20,
-          y: 40,
-          background: am5.Rectangle.new(root, {
-            fill: am5.color(colors.gray[900]),
-            fillOpacity: 0.8,
-          }),
-        })
-      );
-
-      cont.children.push(
-        am5.Label.new(root, {
-          centerY: am5.p50,
-          text: `Today's Upcoming ${sport.toUpperCase()} Schedule`,
-          fontSize: 18,
-        })
-      );
 
       // Create main polygon series for countries
       // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/
-      let polygonSeries = chart.series.push(
+      chart.series.push(
         am5map.MapPolygonSeries.new(root, {
           geoJSON: am5geodata_worldLow,
         })
@@ -125,36 +69,47 @@ export default function App() {
       // https://www.amcharts.com/docs/v5/charts/map-chart/map-line-series/
       let lineSeries = chart.series.push(am5map.MapLineSeries.new(root, {}));
       lineSeries.mapLines.template.setAll({
-        stroke: am5.color(colors.blue[800]),
+        stroke: am5.color(colors.gray[600]),
         tooltipText: "{name}",
-        strokeWidth: 5,
+        tooltipPosition: "pointer",
+        strokeWidth: 3,
         strokeOpacity: 0.6,
       });
+
+      const locations = await getGameLocations(sport);
+
+      const createLogoImage = (logo: string) => {
+        return am5.Picture.new(root, {
+          width: 48,
+          height: 48,
+          centerX: am5.p50,
+          centerY: am5.p50,
+          src: logo,
+        });
+      };
+
       // destination series
       const awayCitySeries = chart.series.push(
         am5map.MapPointSeries.new(root, {})
       );
-      awayCitySeries.bullets.push(function () {
-        let circle = am5.Circle.new(root, {
-          radius: 5,
-          tooltipText: "{title}",
-          tooltipPosition: "pointer",
-          tooltipX: 0,
-          tooltipY: 0,
-          fill: am5.color(0xe43614),
-        });
+      awayCitySeries.bullets.push(function (_, _2, dataItem) {
+        const data = dataItem.dataContext as any;
+
+        const sprite = createLogoImage(data.logo);
+
         return am5.Bullet.new(root, {
-          sprite: circle,
+          sprite,
         });
       });
 
       const homeCitySeries = chart.series.push(
         am5map.MapPointSeries.new(root, {})
       );
-      homeCitySeries.bullets.push(function () {
-        let circle = am5.Circle.new(root, {});
+      homeCitySeries.bullets.push(function (_, _2, dataItem) {
+        const data = dataItem.dataContext as any;
+        const sprite = createLogoImage(data.logo);
         return am5.Bullet.new(root, {
-          sprite: circle,
+          sprite,
         });
       });
 
@@ -164,10 +119,10 @@ export default function App() {
       );
       arrowSeries.bullets.push(function () {
         const arrow = am5.Graphics.new(root, {
-          fill: am5.color(colors.blue[800]),
+          fill: am5.color(colors.gray[900]),
           fillOpacity: 1,
           tooltipText: "{name}",
-          tooltipPosition: "pointer",
+          // tooltipPosition: "pointer",
           draw: function (display) {
             display.moveTo(0, -8);
             display.lineTo(18, 0);
@@ -180,15 +135,18 @@ export default function App() {
         });
       });
 
-      const locations = await getGameLocations(sport);
       const homeCities = locations.map((l) => ({
         id: l.home.name,
         title: l.home.name,
+        color: l.home.color,
+        logo: l.home.logo,
         geometry: { type: "Point", coordinates: [l.home.lon, l.home.lat] },
       }));
       const awayCities = locations.map((l) => ({
         id: l.away.name,
         title: l.away.name,
+        color: l.away.color,
+        logo: l.away.logo,
         geometry: { type: "Point", coordinates: [l.away.lon, l.away.lat] },
       }));
 
@@ -209,16 +167,12 @@ export default function App() {
         });
         arrowSeries.pushDataItem({
           // @ts-expect-error
-          name: location.home.name,
+          name: location.label,
           lineDataItem,
-          positionOnLine: 1,
+          positionOnLine: 0.5,
           autoRotate: true,
         });
       }
-
-      polygonSeries.events.on("datavalidated", function () {
-        chart.zoomToGeoPoint({ longitude: -0.1262, latitude: 51.5002 }, 3);
-      });
 
       chart.appear(1000, 100);
 
@@ -226,27 +180,62 @@ export default function App() {
     };
 
     setupMap();
+
+    return () => {
+      root.dispose();
+    };
   }, [sport]);
+
   return (
     <div className="h-screen overflow-hidden w-screen bg-gray-800 flex justify-center items-center">
-      {loading && (
-        <div className="h-12 absolute bottom-5 right-5 w-12 rounded-full border-l-white border-2 border-t-white flex-shrink-0 border-b-white border-r-transparent animate-spin" />
-      )}
       <div id="chartdiv" className="w-full h-full min-h-screen" />
-      <div className="absolute right-5 top-5 bg-gray-900/80">
-        <select
-          value={sport}
-          onChange={(e) => {
-            setSport(e.currentTarget.value as any);
-          }}
-          className="text-lg bg-gray-900/80 text-white"
-        >
-          <option value="mlb">MLB</option>
-          <option value="nfl">NFL</option>
-        </select>
+      <h1 className="absolute top-4 left-4 text-white text-lg bg-gray-900/80 px-3 py-1 z-10">
+        {sport.toUpperCase()} Travel Map
+      </h1>
+      <div className="absolute right-4 top-4 flex items-center gap-2">
+        {loading && (
+          <div className="h-6 w-6 rounded-full border-l-blue-400 border-2 border-t-blue-400 flex-shrink-0 border-b-blue-400 border-r-transparent animate-spin" />
+        )}
+        <div className="flex items-center  bg-gray-900/80 text-white px-2 py-1 text-lg">
+          <Listbox value={sport} onChange={setSport}>
+            <ListboxButton className={"flex items-center gap-2"}>
+              {sport.toUpperCase()}
+
+              <svg
+                className="w-4 h-4 text-white pointer-events-none"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </ListboxButton>
+            <ListboxOptions
+              anchor="bottom end"
+              className={"bg-gray-900/80 divide-y divide-gray-700 mt-2"}
+            >
+              {Object.entries(sports).map(([key, label]) => (
+                <ListboxOption
+                  key={key}
+                  value={key}
+                  className={
+                    "text-gray-200 px-3 py-1.5 cursor-pointer hover:bg-gray-700"
+                  }
+                >
+                  {label}
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </Listbox>
+        </div>
       </div>
       <a
-        href="https://github.com/andrewrjohn"
+        href="https://weaklytyped.com"
         target="_blank"
         rel="norefferer"
         className="absolute right-3 bottom-3 text-white px-2 py-1 text-sm bg-gray-900/80"
